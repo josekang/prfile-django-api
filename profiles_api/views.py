dc2ee87802ae02f22a181b6d1f9ec3d416d48e35
jose <josekangethe2@gmail.com>
2020-06-02 12:30:12 +0300

    API test

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 # Create your views here.
+
+class HelloAPIView(APIView):
+    """ Return features of an API """
+    def get(self, request, formart=None):
+        """ Shows the methods used """
+        api_views = [
+            "Similar to traditional django mehods",
+            "Methods are GET, POST, PUT, PATCH, DELETE"
+        ]
+        
+        response = {
+            "status": status.HTTP_200_OK,
+            "message": "Shows the output as a dictionary, power of API",
+            "data": api_views
+        }
+        
+        return Response(response)
